751406bb002d71a7a286a4b72d91a653bcdbbb35
Sandeep Saimon <sandeeprajsaimon999@gmail.com>
2024-04-20 21:43:43 +0530

    changes for db config

--- src/db.js
+++ src/db.js
@@ -1,9 +1,16 @@
+
 import { Sequelize } from "sequelize";
 import { DB_CONFIG } from "./config.js/db.js";
 const db = new Sequelize(DB_CONFIG.DB_NAME, DB_CONFIG.DB_USER, DB_CONFIG.DB_PASSWORD, {
     host: DB_CONFIG.DB_HOST,
     dialect: 'postgres',
-    port: DB_CONFIG.DB_PORT
+    port: DB_CONFIG.DB_PORT,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      }
+    }
   })
 
 export default db
